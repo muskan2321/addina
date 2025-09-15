c5f07ee4c8d59c6a2240b57aa9601a472bfb20d4
muskan <muskansinha2321@gmail.com>
2025-09-15 17:43:50 +0530

    Add initial HTML and JavaScript files for Task 2 and Task 3
    - Created index.html and script.js for Task 2, implementing basic arithmetic operations.
    - Created index.html and script1.js for Task 3, displaying a name and age.

---
+++ Task-2/script.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 2;
+document.write(`${a + b}`);
+document.write(`${a - b}`);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ Task-3/script1.js
@@ -0,0 +1,3 @@
+let name = "Muskan";
+let age = 22;
+document.write("My name is" + name);
